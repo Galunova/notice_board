eee3bdaa3d4075a27f640a24320c1ef45c1735f6
Adelina_Galunova <adelina.vitamine@gmail.com>
2017-09-30 14:24:27 +0600

    Add categories

---
+++ app/controllers/categories_controller.rb
@@ -0,0 +1,32 @@
+class CategoriesController < ApplicationController
+
+  def index
+    @categories = Category.all
+  end
+
+  def new
+    @category = Category.new
+  end
+
+  def create
+    @category = Category.new(category_params)
+
+    if @category.save
+      redirect_to root_path
+    else
+      render 'new'
+    end
+  end
+
+  def destroy
+    @category = Category.find(params[:id])
+    @category.destroy
+    redirect_to root_path
+  end
+
+  private
+
+  def category_params
+    params.require(:category).permit(:title)
+  end
+end
