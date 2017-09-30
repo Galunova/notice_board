ce1654bc17667defcac79841c1b65136b67aab21
Adelina_Galunova <adelina.vitamine@gmail.com>
2017-09-30 15:47:26 +0600

    Add seed fot admin, user, category, notice

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+admin = User.create!(email: 'admin@example.com',
+										name: 'Admin' ,
+										password: 'qweqwe', 
+										password_confirmation: 'qweqwe', 
+										admin: true)
+user1 = User.create!(email: 'boris.ogurkis@example.com', 
+										name: 'Boris',
+										password: 'qweqwe', 
+										password_confirmation: 'qweqwe')
+
+category1 = Category.create!(title: 'simple category')
+
+notice = Notice.create!(title: "simple notice",
+									     content: "This is sample notice",
+									     contact_details: "ololo street, 9379992",
+									     user_id: user1.id,
+									     category_id: category1.id)
